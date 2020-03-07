34987c816165fc74c8c6616ecf27bcf003e0e907
casperleerink <casperleerink@gmail.com>
2020-03-07 09:59:34 -0800

    change to cloudinary

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -1,13 +1,14 @@
 import CMS from 'netlify-cms-app';
-
+import cloudinary from 'netlify-cms-media-library-cloudinary';
 import Preview from './preview-templates/preview'
+
 
 import styles from '!css-loader!sass-loader!../layout.scss'
 CMS.registerPreviewStyle(styles.toString(), { raw: true });
 CMS.registerPreviewTemplate('home', Preview);
 CMS.registerPreviewTemplate('about', Preview);
 CMS.registerPreviewTemplate('performances', Preview);
-
+CMS.registerMediaLibrary(cloudinary);
 
 
 
