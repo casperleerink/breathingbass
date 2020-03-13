3639675699eb01fb5ccab1fabe44d94bc9f38e9c
casperleerink <casperleerink@gmail.com>
2020-03-12 18:06:18 -0700

    changed biography layout for mobile

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -6,7 +6,7 @@ import Preview from './preview-templates/preview'
 import styles from '!css-loader!sass-loader!../layout.scss'
 CMS.registerPreviewStyle(styles.toString(), { raw: true });
 CMS.registerPreviewTemplate('home', Preview);
-CMS.registerPreviewTemplate('about', Preview);
+CMS.registerPreviewTemplate('biographies', Preview);
 CMS.registerPreviewTemplate('performances', Preview);
 CMS.registerMediaLibrary(cloudinary);
 
